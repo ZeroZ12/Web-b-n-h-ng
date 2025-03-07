52badd22f93124ce9cef6955deb125b6a782189a
ZeroZ12 <longthph53584@gmail.com>
2025-03-07 10:55:26 +0700

    M

--- Views/clients/categories/category.php
+++
@@ -1,5 +0,0 @@
-<?php include_once ROOT_DIR . "Views/clients/header.php"; ?>
-
-
-
-<?php include_once ROOT_DIR . "Views/clients/header.php"; ?>
--- env.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-const ROOT_DIR = __DIR__ . "/";
-
-const ROOT_URL = "http://localhost/D-n-1/";
-
-const ADMIN_URL = ROOT_URL . "Admin/";
